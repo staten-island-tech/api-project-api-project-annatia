7f970bffc24ceefb30162cceb43b202f357f3f3d
AL1525 <AL1525@users.noreply.github.com>
2021-05-10 12:27:51 -0400

    .

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,19 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 // const key = "YOURKEYHERE";
+
+// var requestUrl = `https://api.jikan.moe/v3/search/anime?q=${query}`;
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}`
+    );
+    const data = await response.json();
+  } catch (error) {
+    console.log(error);
+    alert("There is an error");
+  }
+};
+
+query();
