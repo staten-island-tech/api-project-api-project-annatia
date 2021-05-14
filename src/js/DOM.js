5b3693c8858ae008c1fbbdcc9a4b7813f34c05fa
TiaZheng <tia.zheng22@sitechhs.com>
2021-05-14 08:48:40 -0400

    :(

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,13 @@
+// const DOMSelectors = {
+//   grid: document.querySelector(".anime-grid"),
+// };
 const DOMSelectors = {
-  grid: document.querySelector(".anime-grid"),
+  searchBar: document.getElementById("search-bar"),
+  searchBox: document.getElementById("search-box"),
+  searchButton: document.getElementById("submit-btn"),
+  searchResult: document.getElementById("results"),
 };
 
 export { DOMSelectors };
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
+
+// const key = "YOURKEYHERE";
+
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-// const key = "YOURKEYHERE";
+
+//search bar
+// get query, push into key, display results
+const search = function () {}
+
+
+
+
+
+
+// const query = async function () {
+//     try {
+//       const response = await fetch(
+//         `https://api.jikan.moe/v3/search/anime?q=${query}`
+//       );
+//       const data = await response.json();
+//     } catch (error) {
+//       console.log(error);
+//       alert("There is an error");
+//     }
+//   };
+  
+//   query();
+
+
