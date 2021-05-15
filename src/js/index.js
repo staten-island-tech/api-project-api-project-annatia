17c68c0f587378c661844d0fe218272e3b4a277c
AL1525 <AL1525@users.noreply.github.com>
2021-05-14 21:56:26 -0400

    ?

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,14 @@ const search = function () {};
 
 // const queryResults = DOMSelectors.searchBox.value;
 // console.log(queryResults);
+
+/*
+const searchQuery = ref("");
 const query = async function () {
   try {
-    const response = await fetch(`https://api.jikan.moe/v3/search/anime?q=`);
+    const response = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${searchQuery.value}`
+    );
     const data = await response.json();
     console.log(data.results);
   } catch (error) {
@@ -21,3 +26,22 @@ const query = async function () {
 };
 
 query();
+*/
+
+const animeList = function (setAnimeList) {
+  [""];
+};
+
+const fetchAnime = async (query) => {
+  const queryResult = await fetch(
+    `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=15`
+  ).then((res) => res.json());
+
+  console.log(queryResult.results);
+
+  setAnimeList(queryResult.results);
+};
+
+fetchAnime();
+return setAnimeList;
+animeList();
