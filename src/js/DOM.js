403c80c95ce33ea354c10ac0e59382e4bcd3d6e5
AL1525 <AL1525@users.noreply.github.com>
2021-05-14 20:56:47 -0400

    test

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -6,8 +6,7 @@ const DOMSelectors = {
   searchBox: document.getElementById("search-box"),
   searchButton: document.getElementById("submit-btn"),
   searchResult: document.getElementById("results"),
+  cardTemplate: document.getElementById("animeCard"),
 };
 
 export { DOMSelectors };
-
-
--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,23 @@
-
 // const key = "YOURKEYHERE";
 
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-
 //search bar
 // get query, push into key, display results
-const search = function () {}
+const search = function () {};
 
+// const queryResults = DOMSelectors.searchBox.value;
+// console.log(queryResults);
+const query = async function () {
+  try {
+    const response = await fetch(`https://api.jikan.moe/v3/search/anime?q=`);
+    const data = await response.json();
+    console.log(data.results);
+  } catch (error) {
+    console.log(error);
+    alert("There is an error");
+  }
+};
 
-
-
-
-
-// const query = async function () {
-//     try {
-//       const response = await fetch(
-//         `https://api.jikan.moe/v3/search/anime?q=${query}`
-//       );
-//       const data = await response.json();
-//     } catch (error) {
-//       console.log(error);
-//       alert("There is an error");
-//     }
-//   };
-  
-//   query();
-
-
+query();
